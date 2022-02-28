7c193cc28687882877465ce193ae48e8d6fef580
NaheanBadar <81583547+NaheanBadar@users.noreply.github.com>
2022-02-28 21:19:36 +0100

    Update script.js

--- vue/js/script.js
+++ vue/js/script.js
@@ -65,5 +65,16 @@ function initialize() {
 
 /* FIN JEU */
 
+/* Pour menu apres connexion */
 /*
-test*/
+function openFunction(){
+    document.getElementById("menu").style.width="300px";
+    document.getElementById("mainbox").style.marginLeft="300px";
+    document.getElementById("mainbox").innerHTML="Menu";
+}
+function closeFunction(){
+    document.getElementById("menu").style.width="0px";
+    document.getElementById("mainbox").style.marginLeft="0px";
+    document.getElementById("mainbox").innerHTML="&#9776; GuessUp";
+}
+*/
